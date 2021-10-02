20776548acc35bd89da0e457d3c35d7286df5a9b
suhaantammana <89089472+suhaantammana@users.noreply.github.com>
2021-10-02 13:20:20 -0400

    Add files via upload

---
+++ project-c23-template-main/sketch.js
@@ -0,0 +1,71 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var player, playerBase;
+
+
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  angleMode(DEGREES);
+
+  var options = {
+    isStatic: true
+  };
+
+  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
+  World.add(world, playerBase);
+
+  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
+  World.add(world,player)
+}
+
+function draw() {
+  background(backgroundImg);
+
+  Engine.update(engine);
+  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+  image(playerimage,player.position.x,player.position.y,50,180)
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+}
+if (keyIsDown(DOWN_ARROW) && angle < -73){
+  angle+= 1;
+  Matter.Body.setAngle(this.Body, angle);
+}
+
+if (keyIsDown)(UP_ARROW) && angle > -103){
+  angle-=1;
+  Matter.Body.setAngle(this.Body, angle);
+
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+}
+
